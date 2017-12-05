a5fd46909f109d1af133cb88b8ece770f2df0b32
Epono <guillaume.ambrois@gmail.com>
2017-12-05 01:21:54 +0100

    cool

--- source/Tweaking.hx
+++ source/Tweaking.hx
@@ -2,8 +2,8 @@ package;
 
 class Tweaking 
 {
-	public static inline var MONEY_MULTIPLIER				: Float = 1000;
-	// public static inline var MONEY_MULTIPLIER			: Float = 100000000;
+	//public static inline var MONEY_MULTIPLIER				: Float = 1000;
+	public static inline var MONEY_MULTIPLIER				: Float = 100000000;
 	public static inline var PLAYER_GAME_OVER_MONEY 		: Float = 100 	* MONEY_MULTIPLIER;
 	public static inline var PLAYER_START_MONEY 			: Float =  10 	* MONEY_MULTIPLIER;
 	public static inline var MONEY_PER_SECOND				: Float =  	2.5 * MONEY_MULTIPLIER;
